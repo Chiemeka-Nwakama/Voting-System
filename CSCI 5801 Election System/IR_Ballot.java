dad94bdd337e277c5b4395c33a91cd45a19c1d25
casey526 <casey526@umn.edu>
2023-03-16 19:45:54 -0500

    IR_Ballot file

--- CSCI 5801 Election System/IR_Ballot.java
+++ CSCI 5801 Election System/IR_Ballot.java
@@ -1,19 +1,37 @@
 public class IR_Ballot {
     private int[] ballot;
-    private int currentVote;
-    private int currentCandidate;
+    private int currentVote; //holds the ranking of the current vote
+    private int currentCandidate; //holds the index of the candadate for the current vote
 
     public IR_Ballot(int[] rankings){
-
+        ballot = rankings;
+        for (int a = 0; a <= ballot.length; a++){ //parse through ballot for first vote
+            if (ballot[a] == 1){
+                currentCandidate = a; //assign candidate index to currentCandidate
+                break;
+            }
+        }
+        currentVote = 1;
     }
 
-    public void updateCurrentVote(){
-
+    public void updateCurrentVote(){ //called when candidate is eliminated, updates vote to next rank
+        currentVote += 1;
+        if (currentVote >= ballot.length){ //check if ballot has any votes left
+            currentVote = -1;
+            currentCandidate = -1;
+        }else{
+            for (int a = 0; a < ballot.length; a++){ //parse through ballot for next vote
+                if (ballot[a] == currentVote){
+                    currentCandidate = a; //assign candidate index to currentCandidate
+                    break;
+                }else{
+                    currentCandidate = -1; //if ballot only didn't rank all candidates
+                }
+            }
+        }
     }
 
-    public int getCurrentVote(){
-
-    }
+    public int getCurrentVote(){return currentCandidate;} //returns index of currently voted for candidate
 
     
 }
