a96b67b98998dcc7e29667ecdd5a997ed4f433ac
nguy4257 <nguy4257@umn.edu>
2023-03-20 11:50:16 -0500

    Completed CPL_Ballot

--- CSCI 5801 Election System/CPL_Ballot.java
+++ CSCI 5801 Election System/CPL_Ballot.java
@@ -1,13 +1,20 @@
+import java.util.Arrays;
 
 public class CPL_Ballot {
+    private int ballot[];
     private int partyVote;
  
 
     public CPL_Ballot(int pos, int amountOfParties){
+        ballot = new int[amountOfParties];
+        Arrays.fill(ballot, 0); // fill entire array with 0s
+        ballot[pos] = 1; // set vote position to 1
+        partyVote = pos;
 
     }
 
     public int getPartyVote(){
+        return partyVote;
 
     }
 
