fefe007bb85e2cb502844730c13e8b8a97e0a17d
Chiemeka Nwakama <nwaka013@umn.edu>
2023-03-21 18:35:47 -0500

    Work on CPL poplate data added

--- CSCI 5801 Election System/CPL.java
+++ CSCI 5801 Election System/CPL.java
@@ -13,39 +13,13 @@ public class CPL {
 
     public CPL(File file){
         audit = new CPL_Audit_File(); // intitalzies the audit file
-        try {  
-        Scanner sc = new Scanner(file);
-        sc.nextLine(); //ignores first line since it was read already in the Election Class
-        numParties =  sc.nextInt(); //reads in the 2nd line - number of parties
-        String[] partyNames = sc.nextLine().split(","); // 3rd line -- gets names of the party
-        parties = new Party[numParties];
-        for(int i=0; i < numParties; i++){ //creates parties 
-            partyNames[i] = partyNames[i].trim(); // remove leading space between comma and name : , green party = green party
-            parties[i] = new Party(partyNames[i]);
-        }
-        for(int i = 0; i < numParties; i++){ //reads candidates until all parties are populated
-            String[] Candidate_names =  sc.nextLine().split(",");
-            parties[i].populateCandidates(Candidate_names, Candidate_names.length);
-            
-        }
-        totalSeats =  sc.nextInt(); // reads in the total number of seats avaliable
-        numBallots =  sc.nextInt(); // reads in the total number of ballots in election
-        for(int i = 0; i < numParties; i++){ //reads candidates until all parties are populated
-           
-            parties[i].initilizeBallotCapacity(numBallots); //initialize capacity of ballots for each party to be total ballots in election
-        }
-
-
-        sc.close();
-
-    }
-    catch(IOException e){ 
-        System.out.println("Invalid File"); // catches error
-    }
+        populateData(file); // intitalizes the values from the read in files
 }
     
     void run(File file){
+        
 
+        distributeSeat(); // distributes seats to parties
 
     }
 
@@ -69,6 +43,45 @@ public class CPL {
     
     public void distributeSeat(){
 
+
+    }
+
+    public void populateData(File file){
+        try {  
+            Scanner sc = new Scanner(file);
+            sc.nextLine(); //ignores first line since it was read already in the Election Class
+            numParties =  sc.nextInt(); //reads in the 2nd line - number of parties
+            String[] partyNames = sc.nextLine().split(","); // 3rd line -- gets names of the party
+            parties = new Party[numParties];
+            for(int i=0; i < numParties; i++){ //creates parties 
+                partyNames[i] = partyNames[i].trim(); // remove leading space between comma and name : , green party = green party
+                parties[i] = new Party(partyNames[i]);
+            }
+            for(int i = 0; i < numParties; i++){ //reads candidates until all parties are populated
+                String[] Candidate_names =  sc.nextLine().split(",");
+                parties[i].populateCandidates(Candidate_names, Candidate_names.length);
+                
+            }
+            totalSeats =  sc.nextInt(); // reads in the total number of seats avaliable
+            numBallots =  sc.nextInt(); // reads in the total number of ballots in election
+            for(int i = 0; i < numParties; i++){ //reads candidates until all parties are populated
+               
+                parties[i].initilizeBallotCapacity(numBallots); //initialize capacity of ballots for each party to be total ballots in election
+            }
+
+            while(sc.hasNextLine()){ //reads until the end of file
+                
+
+            }
+    
+    
+           
+    
+}
+catch(IOException e){ 
+    System.out.println("Invalid File"); // catches error
+}
+sc.close();
     }
 
     
