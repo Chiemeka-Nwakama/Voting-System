ba1c3c59be103e06ea27c1950c914f067df2dae6
Chiemeka Nwakama <nwaka013@umn.edu>
2023-03-25 12:30:47 -0500

    first test created

--- CSCI 5801 Election System/ElectionTest.java
+++ CSCI 5801 Election System/ElectionTest.java
@@ -1,5 +1,8 @@
 import java.util.Arrays;
 import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -30,10 +33,18 @@ public class ElectionTest {
   
 @Test 
 public void testPopulateParties(){ //test the populate parties that is used in the constructor to see if it populated the data correctly
-    int expectedPartyNumber =  cpl.getNumParties();
-    int actualPartyNumber = 7;
-    String expectedParties;
-    for()
+    String expectedPartyNumber = "7";
+    String actualPartyNumber = cpl.getNumParties() + "";
+    String ExpectedParties = "Democratic Republican New Wave Reform Green Independent Test ";
+    String actualParties = "";
+    Party[] parties = cpl.getParties();
+    for(Party party: parties){
+        actualParties = actualParties + party.getName() + " ";
+
+    }
+    assertEquals(ExpectedParties, actualParties);
+    assertEquals(expectedPartyNumber, actualPartyNumber);
+
 }
 
 }
