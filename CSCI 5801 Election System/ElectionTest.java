1a6978e0f9cf418fd01314b5f23974bb00675f23
nguy4257 <nguy4257@umn.edu>
2023-03-25 17:32:24 -0500

    didn't change, but necessary to commit

--- CSCI 5801 Election System/ElectionTest.java
+++ CSCI 5801 Election System/ElectionTest.java
@@ -1,95 +1,62 @@
-import java.util.Arrays;
 import java.util.Scanner;
-
-import static org.junit.Assert.assertEquals;
-
+import java.io.*;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
-import org.junit.*;
-import org.junit.runners.MethodSorters;
-import java.io.PrintWriter;
-import java.util.Scanner;
+public class Election{
+    public static void main(String[] args){
+        String fileName;
+        boolean valid = false;
+        Scanner scanner = new Scanner(System.in); //intializes a scanner object
+        File election;
 
+        if(args.length > 1) {
+            // command line, right now assumes file name is correct
+            fileName = args[1];
 
-public class ElectionTest {
-
-    CPL cpl;
-    @Before
-    public void setUp(){
-
-    
-    try{
-        File file = new File("C:\\Users\\chiem\\OneDrive - Marshall Public Schools\\Desktop\\repo-Team3\\CSCI 5801 Election System\\test.txt");
-         cpl = new CPL(file);
-    }
-    catch(IOException e){ 
-        System.out.println("Error File not found!");
-    }
-    }
-
-
-  
-@Test 
-public void testPopulateParties(){ //test the populate parties that is used in the constructor to see if it populated the data correctly
-    String expectedPartyNumber = "7";
-    String actualPartyNumber = cpl.getNumParties() + "";
-    String expectedParties = "Democratic Republican New Wave Reform Green Independent Test ";
-    String actualParties = "";
-    Party[] parties = cpl.getParties();
-    for(Party party: parties){
-        actualParties = actualParties + party.getName() + " ";
-
-    }
-    assertEquals("Testing to see if reads in the right number of Parties", expectedPartyNumber, actualPartyNumber);
-    assertEquals("Testing to see if parties read in match", expectedParties, actualParties);
-   
-
-}
-
-@Test 
-
-public void testPopulateCandidate(){ //test the populate candidates that is used in the constructor to see if it populated the data correctly
-
-    String[] expectedCandiates = {"Foster Volz Pike ", "Green Xu Wang ", "Jacks Rosen ", "McClure Berg ", "Zheng Melvin ", "Peters ", "Tester "};
-    Party[] parties = cpl.getParties();
-    int i = 0;
-    for(Party party: parties){
-        CPL_Candidate[] candidates = party.getCandidates();
-        String actualCandiates = "";
-        for(CPL_Candidate candiate: candidates){
-            actualCandiates = actualCandiates + candiate.getName() + " ";
+        }else{            
+            // prompt user
+            System.out.println("Enter in the name of the election file. Enter 'Exit' to exit program");
+            fileName = scanner.nextLine(); //obtains the file name from user
         }
-        assertEquals("Testing to see if reads in the right candidates for each party", expectedCandiates[i], actualCandiates);
-        i++;
 
-    }
-   
+        while(valid || !(fileName.equals("Exit"))){ // while provided filename is not valid or the user has not exited the loop, keep prompting them
 
-}
+            try {  
+                election = new File(fileName);
+                Scanner file_scan  = new Scanner(election);
+                String type = file_scan.nextLine(); // scans in the first line of the election file to see what type of election it is                
+                if (type.equals("CPL")){
+                    CPL cpl  = new CPL(election);
+                    cpl.run(election);
+                    
+                }else{
+                    
+                    IR ir = new IR(election);
+                    ir.run(election);
+                    
+                }
+                
+                file_scan.close();
+                valid = true;
+                break;
 
-@Test 
-
-public void testPopulateBallots(){ //test the populate ballots that is used in the constructor to see if it populated the data correctly
-
-   String[] expectedBallots = {"Ballot 0: 1,,,,,,", "Ballot 1: ,,,,,,1", "Ballot 2: ,1,,,,,", " Ballot 3: ,,,,1,,", "Ballot 4: ,,,,,1,", " Ballot 5: ,,,1,,,", " Ballot 6: ,,1,,,,", " Ballot 7: 1,,,,,,", " Ballot 8: ,1,,,,, "};
-   CPL_Ballot[] ballots = cpl.getBallots();
-   for(int i = 0; i < expectedBallots.length; i++){
-
-    assertEquals("Testing to see if read in ballots",expectedBallots[i], ballots[i].toString());
-
-   }
-    //.asdsa
-    String expectedSeats = "4";
-    String actualSeats = cpl.getTotalSeats() + "";
-
-    assertEquals("Testing to see if read total number of seats",expectedSeats, actualSeats);
-
-
-
-
-}
-}
-
-
+                         
+            }
+                
+            catch(IOException e){  //catches error that file cannot be open    
+                valid = false;
+                System.out.println("Error! invalid File. Try Again");
+                 // while provided filename is not valid or the user has not exited the loop, keep prompting them
+                System.out.println("Enter in the name of the election file. Enter 'Exit' to exit program ");
+                fileName = scanner.nextLine(); //obtains the new file name from user  
+            }
+            
+            
+        } //while
+        
+    
+    scanner.close();
+    //election.close(); close file
+    
+    } //main
+} //eof
