854aafe10950ffb88baa02532f96fbf20925beba
phuongL <lieu0009@umn.edu>
2023-04-26 02:01:01 -0500

    revision

--- Project2/testing/IR_ElectionTest.java
+++ Project2/testing/IR_ElectionTest.java
@@ -16,12 +16,27 @@ import src.IR_Make_Test_File;
 public class IR_ElectionTest {
 
     IR ir;
+    IR ir_multiple;
+    String path = "/home/lieu0009/5081/repo-Team3/Project2/testing/";
+    
     @Before
     public void setUp(){    
         try{
-            File[] file = new File[1];
-            file[0] = new File("/home/lieu0009/CSCI/CSCI5801/repo-Team3/Project2/testing/IRtest.csv");
-            ir = new IR(file);
+            // File[] file = new File[1];
+            // file[0] = new File("/home/lieu0009/CSCI/CSCI5801/repo-Team3/Project2/testing/IRtest.csv");
+            // ir = new IR(file);
+            File file_1 = new File(path + "IRfile1.csv");
+            File[] file_single = new File[1];
+            file_single[0] = file_1;
+            File[] file_multiple = new File[3];
+            File file_2 = new File(path + "IRfile2.csv");
+            File file_3 = new File(path + "IRfile3.csv");
+            file_multiple[0] = file_1;
+            file_multiple[1] = file_2;
+            file_multiple[2] = file_3;
+
+            ir = new IR(file_single);
+            ir_multiple = new IR(file_multiple);
         }
         catch(Exception e) { 
             System.out.println("Error File not found!");
@@ -43,22 +58,37 @@ public void makeTestFile(){
 public void testInitialData(){ //test the initial data is used in the constructor to see if it populated the data correctly
     String expectednumCandidates = "4";
     String actualnumCandidates = ir.getNumCandidates() + "";
+    String actualnumCandidates_m = ir_multiple.getNumCandidates() + "";
     String expectedCandidates = "Rosen (D) Kleinberg (R) Chou (I) Royce (L) ";
     String actualCandidtates = "";
+    String actualCandidtates_m = "";
     IR_Candidate[] candidates = ir.getCandidates();
+    IR_Candidate[] candidates_m = ir_multiple.getCandidates();
     for(IR_Candidate candidate: candidates){
         actualCandidtates = actualCandidtates + candidate.getName() + " ";
+    }
+    //multiple
+    for(IR_Candidate candidate: candidates_m){
+        actualCandidtates_m = actualCandidtates_m + candidate.getName() + " ";
     }
 
     String expectedBallots = "6";
     String actualBallots = ir.getNumBallots() + "";
+    //multiple
+    String expectedBallots_m = "18";
+    String actualBallots_m = ir_multiple.getNumBallots() + "";
 
     assertEquals("Testing to see if reads in the right number of Candidates", expectednumCandidates, actualnumCandidates);
     assertEquals("Testing to see if reads in the right name of Candidates", expectedCandidates, actualCandidtates);
     assertEquals("Testing to see if reads in the right number of ballots", expectedBallots, actualBallots);
-   
+    //multiple
+    assertEquals("Testing to see if reads in the right number of Candidates", expectednumCandidates, actualnumCandidates_m);
+    assertEquals("Testing to see if reads in the right name of Candidates", expectedCandidates, actualCandidtates_m);
+    assertEquals("Testing to see if reads in the right number of ballots", expectedBallots_m, actualBallots_m);
 
 }
+
+
 
 @Test
 public void testpopulateData(){
@@ -69,9 +99,21 @@ public void testpopulateData(){
         actualBallots[a] = Arrays.toString(ballots[a].getBallot());
     }
 
-
     for(int i = 0; i < expectedBallots.length; i++){
         assertEquals("Testing for correct votes stored: ", expectedBallots[i], actualBallots[i]);
+    }
+    //multiple
+    String[] expectedBallots_m = {"[1, 3, 4, 2]", "[1, 0, 2, 0]", "[1, 2, 3, 0]", "[3, 2, 1, 4]", "[0, 0, 1, 2]", "[0, 0, 0, 1]",
+                                "[1, 3, 4, 2]", "[1, 3, 2, 0]", "[1, 2, 3, 0]", "[3, 2, 1, 4]", "[0, 0, 1, 2]", "[0, 0, 0, 1]",
+                                "[1, 3, 4, 2]", "[1, 0, 2, 0]", "[1, 2, 3, 0]", "[3, 2, 1, 4]", "[0, 0, 0, 1]", "[1, 0, 0, 0]"};
+    IR_Ballot[] ballots_m = ir_multiple.getBallots();
+    String[] actualBallots_m = new String[18];
+    for (int a = 0; a < 18; a++){
+        actualBallots_m[a] = Arrays.toString(ballots_m[a].getBallot());
+    }
+
+    for(int i = 0; i < expectedBallots_m.length; i++){
+        assertEquals("Testing for correct votes stored: ", expectedBallots_m[i], actualBallots_m[i]);
     }
 
 }
@@ -86,6 +128,14 @@ public void testgetCandidates(){
     }
 
     assertEquals("Testing getter: ", expectedCandidates, actualCandidtates);
+    //multiple
+    String actualCandidtates_m = "";
+    IR_Candidate[] candidates_m = ir_multiple.getCandidates();
+    for(IR_Candidate candidate: candidates_m){
+        actualCandidtates_m = actualCandidtates_m + candidate.getName() + " ";
+    }
+
+    assertEquals("Testing getter: ", expectedCandidates, actualCandidtates_m);
 
 }
 
@@ -95,6 +145,10 @@ public void testgetNumCandidates(){
     String actualnumCandidates = ir.getNumCandidates() + "";
     
     assertEquals("Testing getter: ", expectednumCandidates, actualnumCandidates);
+    //multiple
+    String actualnumCandidates_m = ir_multiple.getNumCandidates() + "";
+    
+    assertEquals("Testing getter: ", expectednumCandidates, actualnumCandidates_m);
 }
 
 @Test
@@ -103,6 +157,11 @@ public void testgetNumBallots(){
     String actualBallots = ir.getNumBallots() + "";
 
     assertEquals("Testing getter: ", expectedBallots, actualBallots);   
+    //multiple
+    String expectedBallots_m = "18";
+    String actualBallots_m = ir_multiple.getNumBallots() + "";
+
+    assertEquals("Testing getter: ", expectedBallots_m, actualBallots_m);
 }
 
 @Test
@@ -121,6 +180,10 @@ public void testSetElectionStatus(){
     IR_Candidate[] rankings = ir.getRankings();
     String leadCandidate = "Rosen (D)";
     assertEquals(leadCandidate, rankings[0].getName());
+    //multiple
+    ir_multiple.setElectionStatus();
+    IR_Candidate[] rankings_m = ir_multiple.getRankings();
+    assertEquals(leadCandidate, rankings_m[0].getName());
 }
 
 @Test
@@ -134,7 +197,16 @@ public void testMakeLoser(){
         actualLoser = "Yes";
     }
     assertEquals (expectedLoser, actualLoser);
-}
+    //multiple
+    ir_multiple.setElectionStatus();
+    IR_Candidate[] rankings_m = ir_multiple.getRankings();
+    ir_multiple.makeLoser(rankings[3]);
+    String actualLoser_m = "No";
+    String expectedLoser_m = "Yes";
+    if (!rankings_m[3].getStatus()){
+        actualLoser_m = "Yes";
+    }
+    assertEquals (expectedLoser_m, actualLoser_m);
 
 @Test
 public void testCoinToss(){ //should pass about half the time
@@ -142,6 +214,12 @@ public void testCoinToss(){ //should pass about half the time
     boolean expectedCandidate = true;
     boolean actualCandidate = false;
     if (candidate >= 0 && candidate <= 1){
+        actualCandidate = true;
+    }
+    assertEquals(expectedCandidate, actualCandidate);
+    //multiple
+    int candidate_m = ir_multiple.coinToss();
+    if (candidate_m >= 0 && candidate_m <= 1){
         actualCandidate = true;
     }
     assertEquals(expectedCandidate, actualCandidate);
@@ -156,25 +234,12 @@ public void testPoolSelect(){ //should pass about a third of the time
         actualCandidate = true;
     }
     assertEquals(expectedCandidate, actualCandidate);
-}
-@Test 
-public void testMultipleFiles(){
-    String path ="/home/lieu0009/5081/repo-Team3/Project2/testing/";
-    String[] fileNames = {path + "\\IRfile1.csv", path + "\\IRfile2.csv", path + "\\IRfile3.csv"};
-    File[] files = new File[fileNames.length];
-            for(int i = 0; i < fileNames.length; i++){
-                files[i] = new File(fileNames[i]);
-
-            }
-    IR multIR = new IR(files);
-
-   
-    String expected = fileNames[0] + " " + fileNames[1] + " " + fileNames[2];
-    
-    String actual =  multIR.getFiles();
-
-    assertEquals("Testing bringing in multiple files", expected, actual);
-
+    //multiple
+    int candidate_m = ir_multiple.poolSelect(3);
+    if (candidate_m >= 0 && candidate_m <= 2){
+        actualCandidate = true;
+    }
+    assertEquals(expectedCandidate, actualCandidate);
 }
 
 @Test
